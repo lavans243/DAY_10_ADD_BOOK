319bf46177ce010f3909438d6586b32d30e5fdc4
lavans243 <lavans243@gmail.com>
2022-11-25 19:47:33 +0530

    welcome Day10 UC3

---
+++ src/com/bridgelabz/Contact.java
@@ -0,0 +1,51 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class Contact {
+	
+	static ArrayList<String > FirstName;
+    static ArrayList<String > LastName;
+    static ArrayList<String> Address;
+    static ArrayList<String> City;
+    static ArrayList<String > state;
+    static ArrayList<String > Zip;
+    static ArrayList<String > PhoneNuber;
+    static ArrayList<String > email;
+
+    void One (){
+        this.FirstName = new ArrayList<>();
+    }
+
+    void Two (){
+        this.LastName = new ArrayList<>();
+    }
+
+    void Three (){
+        this.Address = new ArrayList<>();
+    }
+
+    void Five (){
+        this.state = new ArrayList<>();
+    }
+
+    void Four (){
+        this.City = new ArrayList<>();
+    }
+
+    void Six (){
+        this.Zip = new ArrayList<>();
+    }
+
+    void Seven (){
+        this.PhoneNuber = new ArrayList<>();
+    }
+
+    void Eight (){
+        this.email = new ArrayList<>();
+
+	
+    }
+
+}
